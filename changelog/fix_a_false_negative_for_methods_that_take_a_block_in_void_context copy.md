2211c430e43f7dd20bd5deb0ce2f6fef10994d4e
Krishan Bhasin <krishan.bhasin@shopify.com>
2023-01-11 11:08:39 +0000

    Update `Lint/Void` to suggest `each` in place of `map` or `collect` in void context.

---
+++ changelog/fix_a_false_negative_for_methods_that_take_a_block_in_void_context copy.md
@@ -0,0 +1,1 @@
+* [#11426](https://github.com/rubocop/rubocop/pull/11426): Update `Lint/Void` to suggest `each` in place of `map` or `collect` in void context. ([@krishanbhasin-shopify][])
--- lib/rubocop/cop/lint/void.rb
+++ lib/rubocop/cop/lint/void.rb
@@ -47,18 +47,23 @@ module RuboCop
         SELF_MSG = '`self` used in void context.'
         EXPRESSION_MSG = '`%<expression>s` used in void context.'
         NONMUTATING_MSG = 'Method `#%<method>s` used in void context. Did you mean `#%<method>s!`?'
+        REPLACABLE_BY_EACH_MSG = 'Method `#%<method>s` used in void context. Did you mean `#each`?'
 
         BINARY_OPERATORS = %i[* / % + - == === != < > <= >= <=>].freeze
         UNARY_OPERATORS = %i[+@ -@ ~ !].freeze
         OPERATORS = (BINARY_OPERATORS + UNARY_OPERATORS).freeze
         VOID_CONTEXT_TYPES = %i[def for block].freeze
-        NONMUTATING_METHODS = %i[capitalize chomp chop collect compact
-                                 delete_prefix delete_suffix downcase
-                                 encode flatten gsub lstrip map merge next
-                                 reject reverse rotate rstrip scrub select
-                                 shuffle slice sort sort_by squeeze strip sub
-                                 succ swapcase tr tr_s transform_values
-                                 unicode_normalize uniq upcase].freeze
+        NONMUTATING_METHODS_WITH_BANG_VERSION = %i[capitalize chomp chop compact
+                                                   delete_prefix delete_suffix downcase
+                                                   encode flatten gsub lstrip merge next
+                                                   reject reverse rotate rstrip scrub select
+                                                   shuffle slice sort sort_by squeeze strip sub
+                                                   succ swapcase tr tr_s transform_values
+                                                   unicode_normalize uniq upcase].freeze
+        METHODS_REPLACABLE_BY_EACH = %i[collect map].freeze
+
+        NONMUTATING_METHODS = (NONMUTATING_METHODS_WITH_BANG_VERSION +
+                               METHODS_REPLACABLE_BY_EACH).freeze
 
         def on_block(node)
           return unless node.body && !node.body.begin_type?
@@ -126,7 +131,11 @@ module RuboCop
         def check_nonmutating(node)
           return unless NONMUTATING_METHODS.include?(node.method_name)
 
-          add_offense(node, message: format(NONMUTATING_MSG, method: node.method_name))
+          if METHODS_REPLACABLE_BY_EACH.include?(node.method_name)
+            add_offense(node, message: format(REPLACABLE_BY_EACH_MSG, method: node.method_name))
+          else
+            add_offense(node, message: format(NONMUTATING_MSG, method: node.method_name))
+          end
         end
 
         def in_void_context?(node)
